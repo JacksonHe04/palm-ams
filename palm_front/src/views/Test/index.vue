1d87bb36d3393ab3231eecea7d428957f46ba14d
Jackson <JacksonHe04@outlook.com>
2024-11-15 23:23:53 +0800

    前端测试API与构建

---
+++ palm_front/src/views/Test/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Test1 from "@/views/Test/Test1.vue";
+import TestWelcome from "@/views/Test/TestWelcome.vue";
+</script>
+
+<template>
+  <Test1 />
+  <TestWelcome />
+</template>
+
+<style scoped></style>
